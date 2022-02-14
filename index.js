162508c52f16c09a1af19388ed600a9d0202dbf6
westhecool <52859033+westhecool@users.noreply.github.com>
2022-02-14 12:10:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var http = require('http');
 const request = require('request');
 
 http.createServer(function (req, res) {
-request('https://ip.seeip.org/jsonip?', { json: true }, (err, res2, body) => {
+request('https://ip.seeip.org/jsonip?', (err, res2, body) => {
   if (err) { return console.log(err); }
-  res.end(res2);
+  res.end(body);
 });
-}).listen(process.env.PORT);
+}).listen(8080);
