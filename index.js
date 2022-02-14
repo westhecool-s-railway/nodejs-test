e748f2d41422dd330b61901a1c69e77e7736828f
westhecool <52859033+westhecool@users.noreply.github.com>
2022-02-14 12:27:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var http = require('http');
 const request = require('request');
 
 http.createServer(function (req, res) {
+console.log("+1");
 request('http://ip-api.com/json/', (err, res2, body) => {
   if (err) { return console.log(err); }
   res.end(body);
