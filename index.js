93e92f4e9718ee79b77645b8e83d2ce01daf8947
westhecool <52859033+westhecool@users.noreply.github.com>
2022-02-14 12:25:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var http = require('http');
 const request = require('request');
 
 http.createServer(function (req, res) {
-request('https://ip.seeip.org/jsonip?', (err, res2, body) => {
+request('http://ip-api.com/json/', (err, res2, body) => {
   if (err) { return console.log(err); }
   res.end(body);
 });
